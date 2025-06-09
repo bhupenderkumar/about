472952fdec8a325e26942fa3d6ce257d5658590e
bhupenderkumar <firststeppublicschool22@gmail.com>
2025-06-09 10:12:01 +0530

    Initial commit

---
+++ About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const About = () => {
+  return (
+    <div>
+      <h1>Bhupender Kumar Sharma</h1>
+      <p>Senior Full Stack Developer</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<About />, document.getElementById('root'));
